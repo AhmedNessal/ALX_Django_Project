832024bd6159269e1a7108d169e784f0d5db3c7e
AhmedNessal <nessalahmed11@gmail.com>
2026-02-28 21:47:18 +0200

    registration and token authentication
    Implement user registration and token authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import generics, status
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    def post(self, request):
+        user = authenticate(
+            username=request.data.get("username"),
+            password=request.data.get("password")
+        )
+        if not user:
+            return Response({"error": "Invalid credentials"}, status=400)
+
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
